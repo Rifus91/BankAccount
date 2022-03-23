a041bc92afe64d6c78a4088257c32e1aeb6ca639
Иван <Rifus91@mail.ru>
2022-03-23 22:05:24 +0300

    Add project files.

---
+++ BankAccount/BankAccount.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class BankAccount
+{
+    private static int accountNumberSeed = 0000000001;
+    public string Number { get; }
+    public string Owner { get; set; }
+    public decimal Balance { get; }
+
+
+    public BankAccount(string name, decimal initialBalance)
+    {
+        this.Owner = name;
+        this.Balance = initialBalance;
+        this.Number = accountNumberSeed.ToString();
+        accountNumberSeed++;
+    }
+
+
+    public void MakeDeposit(decimal amount, DateTime date, string note)
+    {
+    }
+
+    public void MakeWithdrawal(decimal amount, DateTime date, string note)
+    {
+    }
+}
---
+++ BankAccount/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var account = new BankAccount("IVAN", 1000);
+Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+Console.ReadLine();
