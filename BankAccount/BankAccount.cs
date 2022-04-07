b385359185d40ccceff5c91026c8f8fdb26fa4c6
Иван <Rifus91@mail.ru>
2022-04-07 09:48:08 +0300

    добавили разные типы счетов, и конструкторы (наследовали от базового)

--- BankAccount/BankAccount.cs
+++ BankAccount/BankAccount.cs
@@ -1,74 +1,77 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-public class BankAccount
+namespace BankAccount
 {
-    private static int accountNumberSeed = 0000000001;
-    public string Number { get; }
-    public string Owner { get; set; }
-    public decimal Balance
+    public class BankAccount
     {
-        get
+        private static int accountNumberSeed = 0000000001;
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance
         {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+
+                return balance;
+            }
+        }
+
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+
+            Owner = name;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
+        }
+
+        private List<Transaction> allTransactions = new List<Transaction>();
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+
+        public string GetAccountHistory()
+        {
+            var report = new System.Text.StringBuilder();
+
             decimal balance = 0;
+            report.AppendLine("Date\t\tAmount\tBalance\tNote");
             foreach (var item in allTransactions)
             {
                 balance += item.Amount;
+                report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{balance}\t{item.Notes}");
             }
 
-            return balance;
-        }
-    }
-
-
-    public BankAccount(string name, decimal initialBalance)
-    {
-        this.Number = accountNumberSeed.ToString();
-        accountNumberSeed++;
-
-        Owner = name;
-        MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
-    }
-
-    private List<Transaction> allTransactions = new List<Transaction>();
-
-    public void MakeDeposit(decimal amount, DateTime date, string note)
-    {
-        if (amount <= 0)
-        {
-            throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
-        }
-        var deposit = new Transaction(amount, date, note);
-        allTransactions.Add(deposit);
-    }
-
-    public void MakeWithdrawal(decimal amount, DateTime date, string note)
-    {
-        if (amount <= 0)
-        {
-            throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
-        }
-        if (Balance - amount < 0)
-        {
-            throw new InvalidOperationException("Not sufficient funds for this withdrawal");
-        }
-        var withdrawal = new Transaction(-amount, date, note);
-        allTransactions.Add(withdrawal);
-    }
-
-    public string GetAccountHistory()
-    {
-        var report = new System.Text.StringBuilder();
-
-        decimal balance = 0;
-        report.AppendLine("Date\t\tAmount\tBalance\tNote");
-        foreach (var item in allTransactions)
-        {
-            balance += item.Amount;
-            report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{balance}\t{item.Notes}");
+            return report.ToString();
         }
 
-        return report.ToString();
+
     }
-
-
 }
